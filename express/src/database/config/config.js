728e6094c6afaaff9cbc7217934ef30f31c077f0
Aylen <luluaylen00@gmail.com>
2023-02-15 13:32:03 -0300

    Segundo test para deploy

--- express/src/database/config/config.js
+++ express/src/database/config/config.js
@@ -1,23 +1,23 @@
 module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "pokemart_db",
-    "host": "127.0.0.1",
+    "username": process.env.MYSQLUSER || "root",
+    "password": process.env.MYSQLPASSWORD || null,
+    "database": process.env.MYSQLDATABASE || "pokemart_db",
+    "host": process.env.MYSQLHOST || "127.0.0.1",
     "dialect": "mysql"
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
+    "username": process.env.MYSQLUSER || "root",
+    "password": process.env.MYSQLPASSWORD || null,
+    "database": process.env.MYSQLDATABASE || "pokemart_db",
+    "host": process.env.MYSQLHOST || "127.0.0.1",
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": process.env.MYSQLUSER || "root",
+    "password": process.env.MYSQLPASSWORD || null,
+    "database": process.env.MYSQLDATABASE || "pokemart_db",
+    "host": process.env.MYSQLHOST || "127.0.0.1",
     "dialect": "mysql"
   }
 }
