8f8d0d3a0ec3c9b9221b9c55545d03b352f147f6
Aylen <luluaylen00@gmail.com>
2023-02-15 21:05:54 -0300

    Cuarto intento

--- express/src/database/config/config.js
+++ express/src/database/config/config.js
@@ -5,23 +5,26 @@ config();
 module.exports = {
   "development": {
     "username": process.env.MYSQLUSER || "root",
-    "password": process.env.MYSQLPASSWORD || null,
-    "database": process.env.MYSQLDATABASE || "pokemart_db",
-    "host": process.env.MYSQLHOST || "127.0.0.1",
-    "dialect": "mysql"
+    "password": process.env.MYSQLPASSWORD || "POcce8Z6PRdfMpEkOVFq",
+    "database": process.env.MYSQLDATABASE || "railway",
+    "host": process.env.MYSQLHOST || "containers-us-west-21.railway.app",
+    "dialect": "mysql",
+    "port": process.env.MYSQLPORT || 5671
   },
   "test": {
     "username": process.env.MYSQLUSER || "root",
-    "password": process.env.MYSQLPASSWORD || null,
-    "database": process.env.MYSQLDATABASE || "pokemart_db",
-    "host": process.env.MYSQLHOST || "127.0.0.1",
-    "dialect": "mysql"
+    "password": process.env.MYSQLPASSWORD || "POcce8Z6PRdfMpEkOVFq",
+    "database": process.env.MYSQLDATABASE || "railway",
+    "host": process.env.MYSQLHOST || "containers-us-west-21.railway.app",
+    "dialect": "mysql",
+    "port": process.env.MYSQLPORT || 5671
   },
   "production": {
     "username": process.env.MYSQLUSER || "root",
-    "password": process.env.MYSQLPASSWORD || null,
-    "database": process.env.MYSQLDATABASE || "pokemart_db",
-    "host": process.env.MYSQLHOST || "127.0.0.1",
-    "dialect": "mysql"
+    "password": process.env.MYSQLPASSWORD || "POcce8Z6PRdfMpEkOVFq",
+    "database": process.env.MYSQLDATABASE || "railway",
+    "host": process.env.MYSQLHOST || "containers-us-west-21.railway.app",
+    "dialect": "mysql",
+    "port": process.env.MYSQLPORT || 5671
   }
 }
